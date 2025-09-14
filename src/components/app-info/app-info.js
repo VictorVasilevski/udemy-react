5d9d9c2e91b7793a585119d198dc652b10a1394e
Viktar Vasileuski <victor.vasilev1991@gmail.com>
2025-09-14 22:01:31 +0300

    adding states to components

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-function AppInfo() {
+function AppInfo({totalNum, increaseNum}) {
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {totalNum}</h2>
+            <h2>Премию получат: {increaseNum}</h2>
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,6 @@
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,40 +9,83 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-    const employeesData = [
+function fetchUserData() {
+    // Stub
+    const data = [
         {
             name: "John C.",
             salary: 800,
             increase: true,
-            id: 1,
+            isLiked: true,
         },
         {
             name: "Alex M.",
             salary: 3000,
             increase: false,
-            id: 2,
+            isLiked: false,
         },
         {
             name: "Carl W.",
             salary: 5000,
             increase: false,
-            id: 3,
+            isLiked: false,
         },
-    ]
-    return (
-        <div className="app">
-            <AppInfo />
+    ];
+    for (let item of data) {
+        item.id = uuidv4();
+    }
+    return data;
+}
 
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {employeesData: fetchUserData()}
+    }
+
+    deleteEmployee = id => {
+        this.setState(({employeesData}) => {
+            return {employeesData: employeesData.filter(item => item.id !== id)}
+        })
+    }
+
+    addEmployee = (name, salary) => {
+        this.setState(({employeesData}) => {
+            const newUserData = {name, salary, increase: false, id: uuidv4()};
+            return {employeesData: [...employeesData, newUserData]}
+        })
+    }
+
+    onToggleProp = (id, propName) => {
+        this.setState(({employeesData}) => ({
+            employeesData: employeesData.map((item => {
+                return (item.id === id) ? {...item, [propName]: !item[propName]} : item;
+            }))
+        }))
+    }
+
+    render() {
+        const increaseNum = this.state.employeesData.filter(item => item.increase).length;
+        return (
+            <div className="app">
+                <AppInfo 
+                    totalNum={this.state.employeesData.length}
+                    increaseNum={increaseNum}/>
+
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+
+                <EmployeesList 
+                    data={this.state.employeesData}
+                    onDelete={this.deleteEmployee}
+                    onToggleProp={this.onToggleProp}/>
+                <EmployeesAddForm 
+                    onAdd={this.addEmployee}/>
             </div>
-
-            <EmployeesList data={employeesData}/>
-            <EmployeesAddForm />
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
